95fa4b2ae6f6104e34b5e922b6cc5b7d96540063
Ancala01 <iqblprtma134@gmail.com>
2024-07-07 09:51:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// script.js
-let correctAnswer = "Bolu";
+let correctAnswer = "Dualah";
 
 function checkAnswer() {
     const answer = document.getElementById('answer').value;
@@ -7,9 +6,9 @@ function checkAnswer() {
     const prize = document.getElementById('prize');
 
     if (answer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
-        message.textContent = "Ciee kok tau si!!";
+        message.textContent = "Ciee kok tau si sering ngasih nasbung perkepala ya hehe!!";
         prize.style.display = 'block';
     } else {
-        message.textContent = "Salah kawan makanya kenalan";
+        message.textContent = "Wrong bro,chat admin kalo mau tau";
     }
 }
